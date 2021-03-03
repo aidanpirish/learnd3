1e2a66d144e852cae338dfd25c6a111dc7b7a0f6
Irish <aidanpirish@gmail.com>
2021-03-03 16:57:19 -0500

    working on bar chart

--- d3_face/src/BarChart.js
+++ d3_face/src/BarChart.js
@@ -1,6 +1,7 @@
 import {Component} from 'react';
 import * as d3 from "d3";
-import { select, arc } from 'd3'
+import { csv, scaleLinear, max, scaleBand } from 'd3'
+import data from './data/populations.csv'
 
 class BarChart extends Component {
 
@@ -16,7 +17,42 @@ class BarChart extends Component {
 
         const width = +svg.attr('width')
         const height = +svg.attr('height')
+
+        csv(data).then(data => {
+            data.forEach(d => {
+                d.population = +d.population
+            })
+            this.renderRows(data)
+        })
         
+    }
+
+    renderRows(data) {
+        const svg = d3.select("svg")
+
+        const width = +svg.attr('width')
+        const height = +svg.attr('height')
+
+        //value accessors
+        const xValue = d => d.population
+        const yValue = d => d.country
+
+        const xScale = scaleLinear()
+            .domain([0, max(data, d=> xValue(d))])
+            .range([0, width])
+
+        const yScale = scaleBand()
+            .domain(data.map(d => yValue(d)))
+            .range([0, height])
+
+        const g = svg.append('g')
+            .attr('transform', `translate(${margin.left},${margin.top})`)
+
+        g.selectAll('rect').data(data)
+            .enter().append('rect')
+                .attr("y", d => yScale(yValue(d)))
+                .attr('width', d => xScale(xValue(d)))
+                .attr('height', yScale.bandwidth())
     }
 
     render() {
